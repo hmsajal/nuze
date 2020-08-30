3942ec6b6e5d0ac9cbbb80382985a0eaee7adac2
hmsajal <19496424+hmsajal@users.noreply.github.com>
2020-08-30 17:13:43 +0600

    intermediary2

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,7 +2,6 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import MenuIcon from "@material-ui/icons/Menu";
