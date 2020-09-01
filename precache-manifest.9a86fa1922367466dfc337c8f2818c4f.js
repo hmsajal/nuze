95c8aaf1fc356be6a93b5fb3a71afecf6f3b416a
hmsajal <19496424+hmsajal@users.noreply.github.com>
2020-09-01 19:06:52 +0600

    Updates

---
+++ precache-manifest.9a86fa1922367466dfc337c8f2818c4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdeeb75e60b9bb34f9ea64f30f9c39f4",
+    "url": "/nuze/index.html"
+  },
+  {
+    "revision": "de43986af26ba51016cb",
+    "url": "/nuze/static/css/main.4b99ff04.chunk.css"
+  },
+  {
+    "revision": "1c894b95c0b770ad9926",
+    "url": "/nuze/static/js/2.16f44275.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nuze/static/js/2.16f44275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de43986af26ba51016cb",
+    "url": "/nuze/static/js/main.c73a7b00.chunk.js"
+  },
+  {
+    "revision": "c1346255edbb06524bdc",
+    "url": "/nuze/static/js/runtime-main.ef372725.js"
+  }
+]);
